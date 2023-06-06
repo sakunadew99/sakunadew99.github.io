cd77fc0b99099c5a6fe8c84f8156ab5834af43e8
sakunadew99 <sakunadew99@gmail.com>
2023-06-06 09:46:27 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.querySelector('body').classList.add('active');
+        document.querySelector('html').classList.add('active');
+    }else{
+        document.querySelector('body').classList.remove('active');
+        document.querySelector('html').classList.remove('active');
+    }
+}
+
+var typed = new Typed('.typing-text',{
+    strings : ['front end developer','web designer','graphic designer','web developer'],
+    loop : true,
+    typeSpeed : 150
+});
+
+VanillaTilt.init(document.querySelectorAll('.tilt'),{
+    max : 30
+});
+
+
+
+
+
